8cf1a87e24483cb7488b9979a50b3bfb56648eb6
Fabio Galera <fabiogalera@gmail.com>
2016-07-11 16:53:55 -0300

    Fixing datatabase

--- app/Funcionario.php
+++ app/Funcionario.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Funcionario extends Model
 {
-    protected $fillable =  ['_token', 'nome', 'meio', 'cpf', 'email', 'telefone', 'contato', 'tel_contato', 'rua', 'num', 'comp', 'cep', 'estado', 'cidade', 'nascimento', 'sexo', 'ctps', 'serie', 'tipo', 'cargo', 'salario', 'pis', 'mesa', 'admissao'];
+    protected $fillable =  ['_token', 'nome', 'meio', 'sobrenome', 'cpf', 'email', 'telefone', 'contato', 'tel_contato', 'rua', 'num', 'comp', 'cep', 'estado', 'cidade', 'nascimento', 'sexo', 'ctps', 'serie', 'tipo', 'cargo', 'salario', 'pis', 'mesa', 'admissao'];
     protected $dates = ['nascimento', 'admissao'];
 }
