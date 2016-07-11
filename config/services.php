c3dbe52958d42d95b27c72b131608088428b8bea
fabiogalera <fabiogalera@gmail.com>
2016-07-11 15:29:10 -0300

    Update services.php

--- config/services.php
+++ config/services.php
@@ -42,7 +42,7 @@ return [
     'facebook' => [
         'client_id' => '362352493888460',
         'client_secret' => 'fb5fb79eadd2f930c85cd1a108f3ddbb',
-        'redirect' => 'http://www.mysite.com:8000/auth/facebook/callback',
+        'redirect' => 'http://letseatslave.herokuapp.com/auth/facebook/callback',
     ],
 
 ];
