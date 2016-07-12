f0dfe59c74f162e53e6f7bf76326d2d3c6e304d2
Fabio Galera <fabiogalera@gmail.com>
2016-07-11 23:07:25 -0300

    Adding login

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -15,31 +15,21 @@ class PagesController extends Controller
     protected $redirectPath = '/';
     protected $loginPath = '/login';
 
-    protected $loggedUser;
-
-    public function __construct()
-    {
-        $this->middleware('auth', ['except' => array('logoff','login')]);
-        $this->loggedUser = Auth::user();
-
-    }
-
     public function home () {
-        if (is_null($loggedUser))
+        if (Auth::check())
         {
-            return view('login');
+            return redirect()->route('login');
         }
         return view('welcome');
     }
 
     public function login () {
-        dd($loggedUser);
-        if (is_null($loggedUser))
+        if (Auth::check())
         {
             return view('login');
         }
 
-        return view('welcome');
+        return redirect()->route('/');
     }
 
 }
