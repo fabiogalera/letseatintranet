1d848bf52bb0cf1c2a46099ec1dc113e3347876f
Fabio Galera <fabiogalera@gmail.com>
2016-07-12 15:18:34 -0300

    Wheater Widget

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
 
     <div class="col-md-4 col-sm-4 col-xs-12 arrilot-widget-container">
         <div class="x_panel">
-    {{ AsyncWidget::Weather() }}
+    {{ Widget::Weather() }}
         </div>
     </div>
 
