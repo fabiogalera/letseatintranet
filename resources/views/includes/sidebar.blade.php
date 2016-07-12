e86dfd6ba35c71248b2a68ceb57a3c46b1b46909
Fabio Galera <fabiogalera@gmail.com>
2016-07-12 01:04:11 -0300

    Adding login

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,11 +1,5 @@
 <div class="col-md-3 left_col">
     <div class="left_col scroll-view">
-        <div class="navbar nav_title" style="border: 0;">
-            <a class="site_title"><span>Let's Eat Intranet</span></a>
-        </div>
-
-        <div class="clearfix"></div>
-        <!-- menu profile quick info -->
         <div class="profile">
             <div class="profile_pic">
                 <img src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->name }}" class="img-circle profile_img">
