eae3738574969d8a4d278ffc337c1ad3b092e488
Fabio Galera <fabiogalera@gmail.com>
2016-07-12 18:33:13 -0300

    Wheater Widget

--- resources/views/widgets/weather.blade.php
+++ resources/views/widgets/weather.blade.php
@@ -49,7 +49,7 @@
             <div class="clearfix"></div>
             <div class="row weather-days">
                 @foreach($result->daily->data as $object)
-                    {{!! $array[] = (array) $object }}
+                    {{!! $array = json_decode($object,true) }}
                     {{!! $ShortFormat = Carbon\Carbon::createFromTimestamp($array['time'])->formatLocalized('%a') }}
 
                 <div class="col-sm-2">
