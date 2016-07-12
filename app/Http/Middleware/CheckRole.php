25591e93ea802f9398d176f2ef7d63b0272a4606
Fabio Galera <fabiogalera@gmail.com>
2016-07-12 02:20:59 -0300

    Adding login

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -3,8 +3,18 @@ namespace App\Http\Middleware;
 
 // First copy this file into your middleware directoy
 use Closure;
+use Illuminate\Session\Store;
+
 
 class CheckRole {
+
+    protected $session;
+    protected $timeout = 1;
+
+    public function __construct(Store $session){
+        $this->session = $session;
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -14,7 +24,19 @@ class CheckRole {
      */
     public function handle($request, Closure $next)
     {
-        // Get the required roles from the route
+        $isLoggedIn = $request->path() != 'logout';
+        if(! session('lastActivityTime'))
+            $this->session->put('lastActivityTime', time());
+        elseif(time() - $this->session->get('lastActivityTime') > $this->timeout){
+            $this->session->forget('lastActivityTime');
+            auth()->logout();
+            $parameters = ['message' => 'Sessão expirada', 'level' => 'danger'];
+            return redirect('error')->with($parameters);
+        }
+
+        $isLoggedIn ? $this->session->put('lastActivityTime', time()) : $this->session->forget('lastActivityTime');
+
+            // Get the required roles from the route
         $roles = $this->getRequiredRoleForRoute($request->route());
         // Check if a role is required for the route, and
         // if so, ensure that the user has that role.
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -14,8 +14,6 @@
         <br />
         <br />
         <br />
-        {{ $bag = get('session')->getMetadataBag()->getLifetime() }}
-        <h4> Inativo por {{ $bag }}</h4>
 
         <!-- sidebar menu -->
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
