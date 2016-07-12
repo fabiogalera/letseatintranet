64a7b75a614fa3d06eb06ca2d94646aea9542e8b
Fabio Galera <fabiogalera@gmail.com>
2016-07-11 23:42:49 -0300

    Adding login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
     public function logoff()
     {
         Auth::logout();
-        return redirect('auth/login');
+        return redirect('login');
     }
 
     public function redirectToProvider(Request $request)
