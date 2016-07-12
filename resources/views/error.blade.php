9e7b72c669e04954e1dd443e6d505de3c0ff2301
Fabio Galera <fabiogalera@gmail.com>
2016-07-12 00:32:44 -0300

    Adding login

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -6,6 +6,7 @@
 
 @section('main_container')
     <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
     @if (session()->has('message'))
         <div class="alert alert-{{ session('level') }}">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
@@ -13,6 +14,7 @@
             {{ session('message') }}
         </div>
     @endif
+            </div>
     </div>
 @endsection
 
