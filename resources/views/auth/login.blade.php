6a1c0b1fe53783744927f8a75060fa64cac5c450
Fabio Galera <fabiogalera@gmail.com>
2016-07-12 02:53:12 -0300

    Adding login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,10 +28,7 @@
 
     <div class="login_wrapper">
         <div class="animate form login_form">
-            <section class="login_content center-block">
-                <form>
                     <div class="form-group center-block"><img src="{{ asset("images/logo-lets.png") }}" width="240" height="272"/></div>
-                    <div style="width: 200px;" class="center-block">
                         <br />
 
                         <div class="row">
@@ -49,10 +46,9 @@
                         <br />
                         <br />
                     </div>
-            </section>
         </div>
 
-                    <a href="/auth/facebook" class="btn btn-block btn-social btn-lg btn-facebook">
+                    <a href="/auth/facebook" class="btn btn-block btn-social btn-facebook">
                         <span class="fa fa-facebook"></span> Logar com Facebook
                     </a>
                     <div class="clearfix"></div>
@@ -65,7 +61,6 @@
                             <p>©2016 Todos os direitos reservados. Let's Eat ® - Marca registrada de Marcos Nunes.</p>
                         </div>
                     </div>
-                </form>
     </div>
 </div>
 </body>
